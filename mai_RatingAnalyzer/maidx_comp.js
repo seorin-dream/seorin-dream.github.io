7475a0b8436cb0b1fbae14ad876860b362147625
miiiingho <skyjumpnano@users.noreply.github.com>
2020-08-07 15:48:48 +0900

    Create maidx_comp.js

---
+++ mai_RatingAnalyzer/maidx_comp.js
@@ -0,0 +1,23 @@
+javascript:
+(
+	function(j,f,dom)
+	{
+		j=['in_lv_dxplus.js',
+		   'maidx_comp_data.js'];
+		dom='https://skyjumpnano.github.io/mai_RatingAnalyzer/scripts_maimai/';
+		f=function(s,u)
+		{
+			if(j.length==0)
+			{
+				return;
+			}
+			u=dom+j.shift()+'?'+Date.now();
+			s=document.createElement('script');
+			document.body.appendChild(s);
+			s.charset='UTF-8';
+			s.addEventListener('load',f);
+			s.src=u;
+		};
+		(document.readyState=='loading')?document.addEventListener('DOMContentLoaded',f):f();
+	}
+)();
